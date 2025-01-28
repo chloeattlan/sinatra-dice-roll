7ec44c6252a9d4987c30cdedb12b862e8d3f0018
chloeattlan <164663007+chloeattlan@users.noreply.github.com>
2025-01-28 19:34:41 +0000

    push

--- dice.rb
+++ dice.rb
@@ -8,31 +8,20 @@ BetterErrors.application_root = __dir__
 BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
 get("/") do
-  "
-  <h1>Dice Roll</h1>
-  <ul>
-    <li><a href=\"/dice/2/6\">Roll two 6-sided dice</a></li>
-    <li><a href=\"/dice/2/10\">Roll two 10-sided dice</a></li>
-    <li><a href=\"/dice/1/20\">Roll one 20-sided die</a></li>
-    <li><a href=\"/dice/5/4\">Roll five 4-sided dice</a></li>
-  </ul>
-  "
+  erb(:elephant)
 end
 
-get("/dice/2/6") do
+get '/dice/2/6/' do
   first_die = rand(1..6)
   second_die = rand(1..6)
   sum = first_die + second_die
 
-  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
-
-  "<h1>2d6</h1>
-  <a href=\"/\">back to main screen</a>
-  <p>#{outcome}</p>"
+  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+  erb(:two_six)
 end
 
 
-get("/dice/2/10") do
+get '/dice/2/10/' do
   first_die = rand(1..10)
   second_die = rand(1..10)
   sum = first_die + second_die
@@ -43,15 +32,13 @@ get("/dice/2/10") do
    <p>#{outcome}</p>"
 end
 
-get("/dice/1/20") do
+get '/dice/1/20/' do
   first_die = rand(1..20)
   sum = first_die
 	
-  outcome = "You rolled a #{first_die} for a total of #{sum}."
+  @outcome = "You rolled a #{first_die} for a total of #{sum}."
 	
-  "<h1>1d20</h1>
-  <a href=\"/\">back to main screen</a>
-   <p>#{outcome}</p>"
+  erb(:one_twenty)
 end
 
 get("/dice/5/4") do
